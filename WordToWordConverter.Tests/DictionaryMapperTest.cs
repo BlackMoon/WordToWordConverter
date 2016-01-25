9132c89d726070d39129c6d077a2f2cf6c54e9fc
rrv <admin@mail.ru>
2016-01-25 21:18:39 +0300

    Tests2

--- WordToWordConverter.Tests/DictionaryMapperTest.cs
+++ WordToWordConverter.Tests/DictionaryMapperTest.cs
@@ -1,14 +1,77 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WordToWordConverter.Data;
 
 namespace WordToWordConverter.Tests
 {
     [TestClass]
     public class DictionaryMapperTest
     {
-        [TestMethod]
-        public void TestMethod1()
+        private IDictionaryMapper _dictionaryMapper;
+        private Task _task;
+        [TestInitialize]
+        public void DictionaryMapperTestInit()
         {
+            _dictionaryMapper = new FileDictionaryMapper()
+            {
+                FileName = Path.GetFullPath(@"..\..\..\WordTowordConverter\dictionary.txt"),
+                NeedSort = false
+            };
+
+            _task = _dictionaryMapper.Load();
+        }
+
+        [TestMethod]
+        public void TestGet()
+        {
+            _task.Wait();
+
+            WordItem item = _dictionaryMapper.Get(10);
+            Assert.IsNotNull(item);
+
+            Assert.AreEqual(item.Word, "абаша");
+        }
+
+        [TestMethod]
+        public void TestGetKey()
+        {
+            _task.Wait();
+
+            int? key = _dictionaryMapper.GetKey("абзац");
+            Assert.IsNotNull(key);
+
+            Assert.AreEqual(key, 21);
+        }
+
+        [TestMethod]
+        public void TestFindMutationVariants()
+        {
+            IDictionary<int, int> expected = new Dictionary<int, int>()
+            {
+               { 5113, 1 },
+               { 5427, 1 },
+               { 5499, 3 },
+               { 5448, 2 },
+               { 5454, 2 },
+               { 5469, 2 },
+               { 5479, 2 },
+               { 5485, 2 },
+               { 5493, 2 },
+               { 5502, 3 }
+            };
+
+            _task.Wait();
+
+            IDictionary<int, int> variants = _dictionaryMapper.FindMutationVariants("муха", "слон", 4, 0, new List<int>());
+
+            CollectionAssert.AreEqual((ICollection) variants.Keys, (ICollection) expected.Keys);
+
+            CollectionAssert.AreEqual((ICollection)variants.Values, (ICollection)expected.Values);
         }
     }
 }
---
+++ WordToWordConverter.Tests/StringValidatorTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WordToWordConverter.Validation;
+
+namespace WordToWordConverter.Tests
+{
+    [TestClass]
+    public class StringValidatorTest
+    {
+        [TestMethod]
+        public void TestValidate()
+        {
+            string msg;
+
+            IStringValidator validator = new StringValidator(){ Value = "123"} ;
+            Assert.IsFalse(validator.Validate(out msg));
+
+            validator.Value = "нота1";
+            Assert.IsFalse(validator.Validate(out msg));
+
+            validator.Value = "нора";
+            Assert.IsTrue(validator.Validate(out msg));
+        }
+    }
+}
